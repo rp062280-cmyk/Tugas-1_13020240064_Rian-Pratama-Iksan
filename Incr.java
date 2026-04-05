afcccebfb48953ba1c9b2420aadb49144b6d982e
rp062280-cmyk <rp062280@gmail.com>
2026-04-05 22:16:55 +0800

    Add files via upload

---
+++ Incr.java
@@ -0,0 +1,12 @@
+/* Effek dari operator ++ */
+public class Incr {
+public static void main(String[] args) {
+// TODO Auto-generated method stub
+/* Kamus */
+int i, j;
+/* Program */
+i = 3;
+j = i++;
+System.out.println ("Nilai i : " + (++i) + "\nNilai j : " + j);
+}
+}
